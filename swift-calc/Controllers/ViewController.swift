7b45a231130198090af5462a7e22a08641369836
maestrying <mdwork96@gmail.com>
2024-03-20 18:14:26 +1000

    feat: Layout of the main screen

--- swift-calc/Controllers/ViewController.swift
+++ swift-calc/Controllers/ViewController.swift
@@ -8,12 +8,97 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    
+    // MARK: Numbers
+    
+    @IBAction func zeroBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func oneBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func twoBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func threeBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func fourBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func fiveBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func sixBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func sevenBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func eightBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func nineBtn(_ sender: Any) {
+        
+    }
+    
+    
+    // MARK: Comma
+    
+    @IBAction func commaBtn(_ sender: Any) {
+        
+    }
+    
+    
+    // MARK: Advanced operations
+    
+    @IBAction func clearBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func switchNegativeBtn(_ sender: Any) {
+    }
+    
+    @IBAction func percentBtn(_ sender: Any) {
+    }
+    
+    
+    // MARK: Main operations
+    
+    @IBAction func divideBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func multiplyBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func addBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func subtractBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func equalBtn(_ sender: Any) {
+    }
+    
 }
 
