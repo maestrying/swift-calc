bf9a44193dc66349dfb985b2fd4fbb75bb80777d
maestrying <mdwork96@gmail.com>
2024-03-20 17:42:00 +1000

    Initial Commit

---
+++ swift-calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-calc
+//
+//  Created by Дывак Максим on 20.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
