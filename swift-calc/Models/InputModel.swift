1f977d9e3d0fb197df7b8581327dde95115389b2
maestrying <mdwork96@gmail.com>
2024-03-24 20:35:22 +1000

    refactor: Format the code

--- swift-calc/Models/InputModel.swift
+++ swift-calc/Models/InputModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 
+
 // Set of operations
 enum Operations {
     case add
@@ -18,8 +19,9 @@ enum Operations {
     case none
 }
 
+
 class Calc {
-    var digits: String = "0"
+    private var digits: String = "0"
     private var result: Double = 0
     private var operation: Operations = .none
     private var resetLabel: Bool = false
@@ -28,6 +30,65 @@ class Calc {
     private var firstNum: Double = 0
     private var firstOper: Operations = .none
 
+    
+    // Perform the operation
+    private func doOperation() -> String {
+        operation = .none
+        digits = String(result)
+        if String(result).hasSuffix(".0") {
+            digits = String(Int(result))
+            return String(Int(result))
+        }
+        return String(result)
+    }
+    
+    
+    // Percent
+    private func percent() -> String {
+        switch firstOper {
+        case .add: result = firstNum + result
+        case .subtract: result = firstNum - result
+        case .multiply: result = firstNum * result / 100
+        case .divide: result = firstNum / result
+        case .percent: break
+        case .none: result = firstNum / 100
+        }
+        
+        return doOperation()
+    }
+    
+    
+    // Add the numbers
+    private func add() -> String {
+        result += Double(digits) ?? 0
+        return doOperation()
+    }
+    
+    
+    // Subtract the numbers
+    private func subtract() -> String {
+        result -= Double(digits) ?? 0
+        return doOperation()
+    }
+    
+    
+    // Multiply the numbers
+    private func multiply() -> String {
+        result *= Double(digits) ?? 0
+        return doOperation()
+    }
+    
+    
+    // Divide the numbers
+    private func divide() -> String {
+        if (Int(digits) ?? 0) != 0 {
+            result /= Double(digits) ?? 0
+        } else { return "Ошибка" }
+        
+        return doOperation()
+    }
+    
+    
     // Negation switch
     func negative() -> String {
         if let number = Double(digits) {
@@ -42,6 +103,7 @@ class Calc {
         }
     }
     
+    
     // Add point
     func addPoint() -> String {
         if !comma {
@@ -54,58 +116,7 @@ class Calc {
         
         return digits
     }
-
-    // Perform the operation
-    private func doOperation(res: Double) -> String {
-        operation = .none
-        digits = String(result)
-        if String(result).hasSuffix(".0") {
-            digits = String(Int(result))
-            return String(Int(result))
-        }
-        return String(result)
-    }
     
-    // Percent
-    private func percent() -> String {
-        switch firstOper {
-        case .add: result = firstNum + result
-        case .subtract: result = firstNum - result
-        case .multiply: result = firstNum * result / 100
-        case .divide: result = firstNum / result
-        case .percent: break
-        case .none: result = firstNum / 100
-        }
-        
-        return doOperation(res: result)
-    }
-    
-    // Add the numbers
-    private func add() -> String {
-        result += Double(digits) ?? 0
-        return doOperation(res: result)
-    }
-    
-    // Subtract the numbers
-    private func subtract() -> String {
-        result -= Double(digits) ?? 0
-        return doOperation(res: result)
-    }
-    
-    // Multiply the numbers
-    private func multiply() -> String {
-        result *= Double(digits) ?? 0
-        return doOperation(res: result)
-    }
-    
-    // Divide the numbers
-    private func divide() -> String {
-        if (Int(digits) ?? 0) != 0 {
-            result /= Double(digits) ?? 0
-        } else { return "Ошибка" }
-        
-        return doOperation(res: result)
-    }
     
     // Choice operation
     func operationIs(_ oper: Operations) {
@@ -126,6 +137,8 @@ class Calc {
         result = Double(digits) ?? 0
     }
     
+    
+    // Calculations in the flow
     func equalSquence() -> String {
         guard firstOper != .percent else {
             if String(Double(digits)! / 100).hasSuffix(".0") { return String(Int(digits)! / 100) }
@@ -149,6 +162,7 @@ class Calc {
         return equal()
     }
     
+    
     // Calculate result
     func equal() -> String {
         switch operation {
@@ -160,6 +174,7 @@ class Calc {
         case .none:     return digits
         }
     }
+    
     
     // Print nums in Label
     func printNums(_ num: Int) -> String {
@@ -175,6 +190,7 @@ class Calc {
         return digits
     }
     
+    
     // Clear label
     func clear() -> String {
         digits = "0"
@@ -185,6 +201,7 @@ class Calc {
         firstOper = .none
         return digits
     }
+    
     
     // Delete last symbol
     func clearLastSymb() -> String {
